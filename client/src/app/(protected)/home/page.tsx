a4c1abae46184dea4dda58145e7b9dde596a4cd8
marsaua001 <marsaua001@gmail.com>
2025-08-08 16:10:45 +0300

    fix

--- client/src/app/(protected)/home/page.tsx
+++ client/src/app/(protected)/home/page.tsx
@@ -1,18 +1,16 @@
 import ToDoList from "@/components/ToDoList";
 
 type Props = {
-  page?: string;
-  limit?: string;
+  searchParams: {
+    page?: string;
+    limit?: string;
+  };
 };
 
-export default async function HomePage(searchParams: Props) {
+export default async function HomePage({ searchParams }: Props) {
   const { page, limit } = await searchParams;
   const pageInt = Number(page ?? "1");
   const limitInt = Number(limit ?? "5");
-  console.log(pageInt);
-  console.log(limitInt);
-  // const page = parseInt(searchParams?.page || "1");
-  // const limit = parseInt(searchParams?.limit || "5");
 
   return <ToDoList page={pageInt} limit={limitInt} />;
 }
